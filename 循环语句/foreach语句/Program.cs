36c16d3a40a96a61ea0ae5dc56bd4dff578a6ae1
eddie <eddie@DESKTOP-4JUCR6O>
2018-02-03 16:16:19 +0800

    循环语句

---
+++ 循环语句/foreach语句/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace foreach语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach语句用于遍历数组或对象集合中的元素，只有那些实现了System.Collections.IEnumerable和System.Collections.Generic.IEnumerable<T>接口的数组或对象集合才可以使用
+            int[] array = new int[] { 0, 1, 1, 2, 3, 4 };
+            Console.WriteLine("数组中每个元素如下:");
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 循环语句/for语句/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace for语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+                Console.WriteLine("array[{0}]的值为：{1}",i,i);
+            }
+            Console.Read();
+        }
+    }
+}
