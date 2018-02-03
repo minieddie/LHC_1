292ec91a4856b8fe705f5b92e9ff11bdaea245b6
eddie <eddie@DESKTOP-4JUCR6O>
2018-02-03 17:03:06 +0800

    跳转语句

---
+++ 跳转语句/return/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace return语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //return语句与break的区别：break只退出整个循环，循环体之后的代码依然会被执行；return则不但退出整个循环，还退出了循环所在的函数。
+
+            Console.WriteLine("使用break退出循环的情况：");
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    break;
+                }
+                Console.WriteLine("当前i的值为：{0}", i);
+            }
+            Console.WriteLine("循环已退出！");
+
+            Console.ReadKey();//输入任意按键后执行下方程序，会退出整个Main函数
+
+            Console.WriteLine("使用return退出循环的情况：");
+            for (int i = 0; i < 5; i++)
+            {
+                if (i==2)
+                {
+                    return;//return 退出整个Main函数
+                }
+                Console.WriteLine("当前i的值为：{0}",i);
+            }
+
+            Console.WriteLine("循环已退出！");
+
+            
+        }
+
+        
+    }
+}
---
+++ 跳转语句/跳转语句/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace 跳转语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //break语句：直接退出整个循环
+            //continue语句：立即终止本次循环，但会继续执行下一次循环
+            //goto语句：跳出讯号到已标记好的位置上（为了使代码易于阅读和理解，不建议大家使用goto语句退出循环）
+            //return语句：退出循环和循环所在的函数
+
+            Console.WriteLine("使用continue退出循环的情况：");
+            for (int i = 0; i < 5; i++)
+            {
+                if (i==2)
+                {
+                    continue;
+                }
+                Console.WriteLine("当前i的值为：{0}",i);
+            }
+
+            Console.WriteLine("使用break退出循环的情况：");
+            for (int i = 0; i < 5; i++)
+            {
+                if (i==2)
+                {
+                    break;
+                }
+                Console.WriteLine("当前i的值为：{0}",i);
+            }
+
+            Console.Read();
+        }
+    }
+}
