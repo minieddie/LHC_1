04dd06d8fc9f0925d523459ccb6f00521f9b295e
eddie <eddie@DESKTOP-4JUCR6O>
2018-02-03 13:48:37 +0800

    IF语句

---
+++ If语句/If语句/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace If语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int x = 13;
+
+            if (condition)
+            {
+                Console.WriteLine("变量设置为真");
+            }
+            else
+            {
+                Console.WriteLine("变量设置为假");
+            }
+
+            if (x<5)
+            {
+                Console.WriteLine("x的值小于5！");
+            }
+            else if (x<10)
+            {
+                Console.WriteLine("x的值大于5小于10！");
+            }
+            else if (x<20)
+            {
+                Console.WriteLine("x的值大于10小于20！");
+            }
+            Console.ReadKey();
+        }
+    }
+}
