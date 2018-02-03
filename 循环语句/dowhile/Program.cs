2275f4f8c16a1071d9d98d4ed83d1ecb83c16474
eddie <eddie@DESKTOP-4JUCR6O>
2018-02-03 16:06:26 +0800

    do-while

---
+++ 循环语句/dowhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            Console.WriteLine("while语句的执行结果为：");
+            while (i<5)//先判断再做
+            {
+                Console.WriteLine("当前i的值为：{0}",i);
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("do-while语句的执行结果为:");
+            do//先做一遍再判断
+            {
+                Console.WriteLine("当前i的值为：{0}", i);
+                i++;
+            } while (i<5);
+            Console.Read();
+        }
+    }
+}
---
+++ 循环语句/循环语句/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 循环语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i<5)
+            {
+                Console.WriteLine("当前i的值为：{0}",i);
+                i++;
+            }
+            Console.Read();
+        }
+    }
+}
